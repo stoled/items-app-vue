85c9138b49e1517fed75d27bd74c5525c483bc7e
codeshootah <vnukovg@gmail.com>
2019-10-22 17:39:05 +0300

    add router

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,24 @@
   <div id="app">
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-container>
-        <b-navbar-brand href="#">Items App</b-navbar-brand>
+        <b-navbar-brand to="/">Items App</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#">Home</b-nav-item>
-            <b-nav-item href="#">About</b-nav-item>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
 
       </b-container>
     </b-navbar>
+
+    <b-container>
+      <router-view />
+    </b-container>
+
   </div>
 </template>
 
